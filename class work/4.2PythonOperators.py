a28d773d9ca50975bdec9919bb44bb43aa28a5a4
Priya-345 <goudgpriyanka@gmail.com>
2025-07-07 19:05:44 +0530

    4.PythonOperators.py

---
+++ class work/4.2PythonOperators.py
@@ -0,0 +1,9 @@
+#2. Comparision Operators
+c=56
+d=23
+print('Equal to(c=d):',c==d) #Equal to(c=d): False
+print(' Not Equal to(c!=d):',c!=d) #Not Equal to(c!=d): True
+print('Greater  than(c>d):',c>d) #Greater  than(c>d): True
+print('Less than(c<d):',c<d) #Less than(c<d): False
+print('Greater than or Equal to (c>=d):',c>=d) #Greater than or Equal to (c>=d): True
+print('less than or Equal to(c<=d):',c<=d) #less than or Equal to(c<=d): False
---
+++ class work/4.PythonOperators.py
@@ -0,0 +1,10 @@
+#1.Arithemetic Operators
+a = 20
+b = 30
+print('Addition(a+b):',a+b)  # Addition(a+b): 50
+print('Subraction(a-b):',a-b) #Subraction(a-b): -10
+print('Multiplication(a*b):',a*b) #Multiplication(a*b): 600
+print('Division(a/b):',a/b) #Division(a/b): 0.6666666666666666
+print('Floor Division(a//b):',a//b) #Floor Division(a//b): 0
+print('Modulus(a%b):',a%b) #Modulus(a%b): 20
+print('Exponentiation(a**b):',a**b) #Exponentiation(a**b): 1073741824000000000000000000000000000000
