ea323d80b9bd58ed6a2077a41d5f9a14acc99d25
Priya-345 <goudgpriyanka@gmail.com>
2025-07-07 19:09:41 +0530

    4.2PythonOperators

--- class work/4.PythonOperators.py
+++ class work/4.PythonOperators.py
@@ -7,4 +7,3 @@ print('Multiplication(a*b):',a*b) #Multiplication(a*b): 600
 print('Division(a/b):',a/b) #Division(a/b): 0.6666666666666666
 print('Floor Division(a//b):',a//b) #Floor Division(a//b): 0
 print('Modulus(a%b):',a%b) #Modulus(a%b): 20
-print('Exponentiation(a**b):',a**b) #Exponentiation(a**b): 1073741824000000000000000000000000000000
