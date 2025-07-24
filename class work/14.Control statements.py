530236b50568c885ea06668ab01e0715b924507c
Priya-345 <goudgpriyanka@gmail.com>
2025-07-24 20:46:26 +0530

    Ass1

--- class work/14.Control statements.py
+++ class work/14.Control statements.py
@@ -100,5 +100,30 @@ while n>0:
 print(s)
 
 #
+n=int(input())
+temp=0
+rev = 0
+while n>0:
+    rev=rev*10+(n%10)
+    n//=10
+if rev==temp:
+    print("Palindrome")
+else:
+    print("Not palindrome")
+
+
+#
+n=input()
+full=len(n)-1
+length=len(n)//2
+ind=0
+while ind<=length:
+    if n[ind]!=n[full]:
+        print("Not palindrome")
+        break
+    ind+=1
+    full=-1
+else:
+    print('Palindrome')
 
 
